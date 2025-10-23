5c26a7d080ea0c96398d711463f3c5a57a29e0f3
HARSHIN RAMESH <harshinharshi123@gmail.com>
2025-10-24 02:10:26 +0530

    getting details from DB

---
+++ project-3/app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Depends
+from database import engine, SessionLocal
+from typing import Annotated
+from sqlalchemy.orm import Session
+
+import models
+
+app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get("/")
+async def read_all(db: db_dependency):
+    return db.query(models.Todos).all()
