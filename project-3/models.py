21bd16043dfea990c34cb88cd466c806aa0d0bf1
HARSHIN RAMESH <harshinharshi123@gmail.com>
2025-10-25 13:58:06 +0530

    project 3

--- project-3/models.py
+++ project-3/models.py
@@ -2,9 +2,9 @@ from database import Base
 from sqlalchemy import Column, Integer, String, Boolean
 
 class Todos(Base):
-    __tablename__ = 'todos'
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    description = Column(String)
-    priority = Column(Integer)
-    complete = Column(Boolean, default=False)
+    __tablename__: str = 'todos'
+    id: Column[int] = Column(Integer, primary_key=True, index=True)
+    title: Column[str] = Column(String)
+    description: Column[str] = Column(String)
+    priority: Column[int] = Column(Integer)
+    complete: Column[bool] = Column(Boolean, default=False)
